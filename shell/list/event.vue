5186da844cee99393e4d133f44faed4a800b9626
Francesco Torchia <francesco.torchia@suse.com>
2024-02-27 16:01:42 +0100

    Add Event List component and re-enable e2e tests
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

---
+++ shell/list/event.vue
@@ -0,0 +1,35 @@
+<script>
+import ResourceTable from '@shell/components/ResourceTable';
+import ResourceFetch from '@shell/mixins/resource-fetch';
+
+export default {
+  name:       'ListEvent',
+  components: { ResourceTable },
+  mixins:     [ResourceFetch],
+  props:      {
+    resource: {
+      type:     String,
+      required: true,
+    },
+
+    schema: {
+      type:     Object,
+      required: true,
+    },
+  },
+
+  async fetch() {
+    await this.$fetchType(this.resource);
+  }
+};
+</script>
+
+<template>
+  <ResourceTable
+    :schema="schema"
+    :rows="rows"
+    :headers="$attrs.headers"
+    :group-by="$attrs.groupBy"
+    :loading="loading"
+  />
+</template>
