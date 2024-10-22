d4cfccd9a3052c182fc526d2e295c62eefe683d1
Francesco Torchia <francesco.torchia@suse.com>
2024-10-22 10:54:55 +0200

    Remove shell Cypress
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- cypress/e2e/po/components/resource-table.po.ts
+++
@@ -1,8 +0,0 @@
-import ComponentPo from '@/cypress/e2e/po/components/component.po';
-import SortableTablePo from '@/cypress/e2e/po/components/sortable-table.po';
-
-export default class ResourceTablePo extends ComponentPo {
-  sortableTable() {
-    return new SortableTablePo(this.self());
-  }
-}
