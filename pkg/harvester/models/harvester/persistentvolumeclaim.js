1c55c2ede20c61b68600ea0823fc9e5387becd4c
andy.lee <andy.lee@suse.com>
2025-01-03 13:44:14 +0800

    fix: isEncrypted breaks all volume page
    Signed-off-by: andy.lee <andy.lee@suse.com>

--- pkg/harvester/models/harvester/persistentvolumeclaim.js
+++ pkg/harvester/models/harvester/persistentvolumeclaim.js
@@ -229,7 +229,7 @@ export default class HciPv extends HarvesterResource {
   }
 
   get isEncrypted() {
-    return this.relatedPV?.spec.csi.volumeAttributes.encrypted === 'true';
+    return this.relatedPV?.spec?.csi?.volumeAttributes?.encrypted === 'true';
   }
 
   get longhornVolume() {
