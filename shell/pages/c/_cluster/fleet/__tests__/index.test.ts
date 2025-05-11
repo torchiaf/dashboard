0c8db794e87a6a8b8bf861d622623910f52a6692
Francesco Torchia <francesco.torchia@suse.com>
2025-05-11 13:53:34 +0200

    Fleet enable helmOps in dashbaord page; fix e2e tests
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- shell/pages/c/_cluster/fleet/__tests__/index.test.ts
+++ shell/pages/c/_cluster/fleet/__tests__/index.test.ts
@@ -268,10 +268,13 @@ const gitRepos = [
   }
 ];
 
+const mockedPresetMixin = { methods: { preset: jest.fn() } };
+
 const requiredSetup = (computed = {}, dataProps = {}) => {
   return {
     global: {
-      mocks: {
+      mixins: [mockedPresetMixin],
+      mocks:  {
         $store: {
           getters: {
             currentProduct:         { inStore: 'cluster' },
@@ -292,6 +295,7 @@ const requiredSetup = (computed = {}, dataProps = {}) => {
       ...computed
     },
     data: () => ({
+      presetVersion:         '',
       permissions:           {},
       FLEET,
       [FLEET.GIT_REPO]:      [],
